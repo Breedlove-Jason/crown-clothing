867b55fa1558bee1219c18ddd902b16e57ff400f
Jason Breedlove <j@jasonbreedlove.com>
2022-05-31 12:03:46 -0700

    moved navigation to separate component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import "./components/category-menu/categories.menu.style.scss";
 import Home from "./routes/home/home.component";
-import { Outlet, Route, Routes } from "react-router-dom";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navigation Component</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
-  return <h1>I am the shop</h1>;
+  return <h1>Shop Component</h1>;
 };
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route path="home" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
