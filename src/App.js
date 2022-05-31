18c0483557a5917252c55dc51822acca5aa0cb62
Jason Breedlove <j@jasonbreedlove.com>
2022-05-31 13:30:40 -0700

    sign in added to navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./components/category-menu/categories.menu.style.scss";
 import Home from "./routes/home/home.component";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
